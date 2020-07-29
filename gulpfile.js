9f691a5add7a66581c5f1f7d29fd8bdfdbe938eb
David Huveau <d.huveau@gmail.com>
2020-07-29 09:01:14 +0200

    minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const gulp = require("gulp");
 const concat = require("gulp-concat");
+const minify = require("gulp-minify");
 
 const JS_SOURCES_FILES = "assets/scripts/**/*.js";
 const JS_BUILD_FOLDER = "public/build/scripts";
 
-gulp.task("default", function () {
-
+gulp.task("default",function(){
 });
 
-gulp.task("scripts", function() {
+gulp.task("pack-js", function() {
+  // return gulp.src(JS_SOURCES_FILES)
+  //   .pipe(concat("scripts.js"))
+  //   .pipe(gulp.dest(JS_BUILD_FOLDER));
   return gulp.src(JS_SOURCES_FILES)
-      .pipe(concat("scripts.js"))
-      .pipe(gulp.dest(JS_BUILD_FOLDER));
+    .pipe(concat("app.bundle.js"))
+    .pipe(minify())
+    .pipe(gulp.dest(JS_BUILD_FOLDER));
 });
