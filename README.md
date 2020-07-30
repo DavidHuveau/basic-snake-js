02ad1029ef78b4b8a26cdebc90291c7777ddb639
David Huveau <d.huveau@gmail.com>
2020-07-30 14:50:07 +0200

    concat js files and concat css files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 original source before rework:
 https://mitchum.blog/how-to-build-snake-with-javascript/
+
+gulp source:
+https://codehangar.io/concatenate-and-minify-javascript-with-gulp/
+https://www.toptal.com/javascript/optimize-js-and-css-with-gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,35 @@
 const gulp = require("gulp");
 const concat = require("gulp-concat");
 const minify = require("gulp-minify");
+const cleanCss = require("gulp-clean-css");
 
-const JS_SOURCES_FILES = "assets/scripts/**/*.js";
+const JS_SOURCES_FOLDER = "assets/scripts";
 const JS_BUILD_FOLDER = "public/build/scripts";
 
-gulp.task("default",function(){
-});
+const CSS_SOURCES_FILES = "assets/styles/**/*.css";
+const CSS_BUILD_FOLDER = "public/build/styles";
+
+const jsSourcesFiles = [
+  "utilities",
+  "cell",
+  "board",
+  "snake",
+  "listenForInput",
+  "game",
+  "index"
+];
 
 gulp.task("pack-js", function() {
-  // return gulp.src(JS_SOURCES_FILES)
-  //   .pipe(concat("scripts.js"))
-  //   .pipe(gulp.dest(JS_BUILD_FOLDER));
-  return gulp.src(JS_SOURCES_FILES)
+  const sourcesFilesOrdered = jsSourcesFiles.map(fileName => `${JS_SOURCES_FOLDER}/${fileName}.js`);
+  return gulp.src(sourcesFilesOrdered)
     .pipe(concat("app.bundle.js"))
     .pipe(minify())
     .pipe(gulp.dest(JS_BUILD_FOLDER));
+});
+
+gulp.task("pack-css", function () {
+  return gulp.src(CSS_SOURCES_FILES)
+      .pipe(concat("stylesheet.css"))
+      .pipe(cleanCss())
+      .pipe(gulp.dest(CSS_BUILD_FOLDER));
 });
